4dfc8919644d09a772ce4ed0f2b274f28ea881c9
Admin <admin@Admins-MacBook-Air.local>
2022-08-19 22:09:01 +0300

    Created MainScreen

---
+++ app/src/main/java/com/example/shopinglist/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.shopinglist
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+}
--- app/src/main/java/com/example/shopinglist/screens/MainScreen.kt
+++ app/src/main/java/com/example/shopinglist/screens/MainScreen.kt
@@ -1,15 +1,18 @@
 package com.example.shopinglist.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,19 +32,42 @@ fun MainScreen() {
             {
                 Text(
                     text = "Bananas",
-                    fontSize = 24.sp
+                    fontSize = 24.sp,
+                    color = Color.Black
                     )
                 Text(
                     text = "20",
-                    fontSize = 24.sp
+                    fontSize = 24.sp,
+                    color = Color.Black
                     )
             }
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween) {
-                Icon(
-                    imageVector = Icons.Rounded.Delete,
-                    contentDescription = "Delete"
-                )
+                Row(modifier = Modifier.fillMaxWidth(0.8f)) {
+                    Icon(
+                        imageVector = Icons.Filled.Delete,
+                        contentDescription = "Delete",
+                        modifier = Modifier.size(32.dp),
+                        tint = MaterialTheme.colors.primary
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                    ) {
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = "Plus",
+                        modifier = Modifier.size(32.dp),
+                        tint = MaterialTheme.colors.primary
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.Refresh,
+                        contentDescription = "Plus",
+                        modifier = Modifier.size(32.dp),
+                        tint = MaterialTheme.colors.primary
+                    )
+                }
             }
         }
     }
